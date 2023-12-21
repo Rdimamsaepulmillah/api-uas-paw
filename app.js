673de08a358f631d76599f7c98f248c936c9d574
rdimam <rdsaepulmillah@gmail.com>
2023-12-21 23:27:45 +0700

    second commit

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
