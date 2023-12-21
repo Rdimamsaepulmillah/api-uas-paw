8b9564df38fd0a18e0e51ea7bd9d5ffbc75410f4
rdimam <rdsaepulmillah@gmail.com>
2023-12-21 23:18:57 +0700

    first commit

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./db.js"
-import { getSiswa, createSiswa, updateSiswa, deleteSiswa } from "./api.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db.js";
+import { getSiswa, createSiswa, updateSiswa, deleteSiswa } from "./api.js";
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const corsOptions = {
+  origin: "https://sityas.pandawadev.cloud",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // API routes
-app.get("/daftar", getSiswa)
-app.post("/daftar", createSiswa)
-app.patch("/daftar/:id", updateSiswa)
-app.delete("/daftar/:id", deleteSiswa)
+app.get("/daftar", getSiswa);
+app.post("/daftar", createSiswa);
+app.patch("/daftar/:id", updateSiswa);
+app.delete("/daftar/:id", deleteSiswa);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(
-    `Server is listening on http://localhost:${process.env.PORT || 3000}`,
-  )
-})
+    `Server is listening on http://localhost:${process.env.PORT || 3000}`
+  );
+});
