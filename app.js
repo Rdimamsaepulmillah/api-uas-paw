5588300c5686c330f45d6131d5e7814c83bfca8a
rdimam <rdsaepulmillah@gmail.com>
2023-12-21 23:33:14 +0700

    second commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
